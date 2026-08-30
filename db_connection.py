81893017db78e67a6a424d3f15d7b31f59d036a5
Maximiliano Cosia <maximilianocosia@MacBook-Pro-de-Maximiliano.local>
2026-08-30 15:31:37 -0400

    Configuracion de conexion a base de datos

--- db_connection.py
+++ db_connection.py
@@ -1,11 +1,22 @@
+import os
 import mysql.connector
 from mysql.connector import Error
 
+ssl_ca = None
+for path in ['/etc/ssl/cert.pem', '/etc/ssl/certs/ca-certificates.crt', '/etc/pki/tls/certs/ca-bundle.crt']:
+    if os.path.exists(path):
+        ssl_ca = path
+        break
+
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '',
-    'database': 'mcosia_s12'
+    'host': os.environ.get('DB_HOST', 'gateway01.sa-east-1.prod.aws.tidbcloud.com'),
+    'port': int(os.environ.get('DB_PORT', 4000)),
+    'user': os.environ.get('DB_USER', 'LDeCrEwcxCXM3W7.root'),
+    'password': os.environ.get('DB_PASSWORD', 'c57uT99CtBhr4B7s'),
+    'database': os.environ.get('DB_NAME', 'test'),
+    'ssl_ca': ssl_ca,
+    'ssl_verify_cert': True,
+    'ssl_verify_identity': True
 }
 
 def obtener_conexion():
